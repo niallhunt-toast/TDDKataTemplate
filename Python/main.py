83c845cf0e240760224e7feb38d990acb8f67006
niallhunt-toast <niall.hunt@toasttab.com>
2025-02-05 16:35:53 +0000

    Add scaffolding for python project

---
+++ Python/main.py
@@ -0,0 +1,6 @@
+def example_function():
+    return 42
+
+
+if __name__ == "__main__":
+    pass
---
+++ Python/test_main.py
@@ -0,0 +1,6 @@
+from main import *
+
+
+def test_example_function():
+    expected_value = 42  # Replace with the actual expected value
+    assert example_function() == expected_value
